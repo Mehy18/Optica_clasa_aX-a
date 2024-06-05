53f8de4ab0247e00d7f42d6dc3a1d401a0162cea
Mehy18 <mihailuminosul@gmail.com>
2024-06-05 21:27:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('myButton');
+const video = document.getElementById('youtube-player');
+
+button.addEventListener('click', () => {
+  video.style.display = 'block';
+});
